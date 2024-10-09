15d1b75dc99562214f33a4e2ec307185ffa6e223
Nico Ismaili <nico.ismaili@aoe.com>
2024-10-09 12:58:51 +0200

    chore: removed astro base
    /spend 1m

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-	site: `https://nico-i.github.io/`,
-	base: `bmarks.nico.ismaili.de`,
 	integrations: [tailwind({ applyBaseStyles: false })],
 });
